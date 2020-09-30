18b265801b481024501e759422d6110b0d7b6e3e
Juan David Rueda Quiroga <jrueda@oneago.com>
2020-09-30 10:18:18 -0500

    Changed string main array

--- Ejercicios-17-09-2020/src/Main.java
+++ Ejercicios-17-09-2020/src/Main.java
@@ -2,7 +2,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] arg) {
+    public static void main(String... arg) {
         Scanner key = new Scanner(System.in);
         boolean exit = false;
 
